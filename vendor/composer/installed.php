b7500c9c02eede097efa4c82200864749d9e0bc7
Johann Schopplich <mail@johannschopplich.com>
2025-01-27 18:13:15 +0100

    chore: release v0.2.5

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'johannschopplich/kirby-lingohub',
-        'pretty_version' => '0.2.4',
-        'version' => '0.2.4.0',
+        'pretty_version' => '0.2.5',
+        'version' => '0.2.5.0',
         'reference' => null,
         'type' => 'kirby-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -20,8 +20,8 @@
             'dev_requirement' => false,
         ),
         'johannschopplich/kirby-lingohub' => array(
-            'pretty_version' => '0.2.4',
-            'version' => '0.2.4.0',
+            'pretty_version' => '0.2.5',
+            'version' => '0.2.5.0',
             'reference' => null,
             'type' => 'kirby-plugin',
             'install_path' => __DIR__ . '/../../',
